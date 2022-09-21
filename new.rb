a368e8af03edb54d7fbcb1c588e9cb56c2cc62f8
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-09-21 12:29:15 +0000

    [Start]Rubyを学ぼう

---
+++ new.rb
@@ -0,0 +1,31 @@
+class Car
+  
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
+  def turn(bend)
+    puts "#{bend}に曲がります"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+
+end
+
+a=Car.new
+a.move("右",5)
+
+car=Car.new
+car.run(5)
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります"
+  end
+end
+
+Car.run(10)
+
